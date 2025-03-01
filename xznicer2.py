6d73d87f8b607e4742c36289afb816668bf5f251
Autumn Ives <146475002+lpn256@users.noreply.github.com>
2025-02-28 18:48:37 -0800

    Update xznicer2.py

--- xznicer2.py
+++ xznicer2.py
@@ -9,12 +9,16 @@ results = []
 
 
 def xznicer2(inputfile):
+    totalsteps = 0
     nicevalues = list(range(2, 274))
     for idx, nice in enumerate(nicevalues):
         outputfile = f"/tmp/{inputfile}.nice{nice}.xz"  # Improved naming
         t1 = threading.Thread(target=execnicer, args=(inputfile, outputfile, nice))
         t1.start()
         t1.join()
+        totalsteps+=1
+        print(f"Steps: {totalsteps}/272")
+
     results.sort(key=lambda x: x[0])
     for row in results:
         print(row)
@@ -28,7 +32,7 @@ def execnicer(inputfile, outputfile, nice):
     with open(inputfile, "rb") as fin:  # read bytes mode
         bytes = fin.read()
     cbytes = lzma.compress(bytes, filters=[{
-        "id": lzma.FILTER_LZMA2, "nice_len": nice, "dict_size": 2**20, "lc": 3, "lp": 0, "pb": 2, "mode": lzma.MODE_FAST
+        "id": lzma.FILTER_LZMA2, "nice_len": nice, "dict_size": 2**20, "lc": 0, "lp": 0, "pb": 0, "mode": lzma.MODE_NORMAL, "preset": 9
     }])
     cbytes += lzma.LZMACompressor().flush()
     with lzma.open(outputfile, "w") as fout:
